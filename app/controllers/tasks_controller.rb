0b7cc117bcda55a8e6ae169d0ad2913c6600f262
Horge <hhoruhe@gmail.com>
2020-04-12 05:11:50 +0000

    TaskList

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,58 @@
+class TasksController < ApplicationController
+  def index 
+    @tasks = Task.all
+  end
+  
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new 
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    
+    if @task.save
+      flash[:success] = "Taskが登録されました"
+      redirect_to @task
+    else 
+      flash.now[:danger] = "Taskの登録に失敗しました"
+      render :new
+    end
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update 
+    @task = Task.find(params[:id])
+    
+    if @task.update(task_params)
+      flash[:success] = "Taskが更新されました"
+      redirect_to @task
+    else 
+      flash.now[:danger] = "更新に失敗しました"
+      render :edit
+    end
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    
+    flash[:success] = "Taskを削除しました"
+    redirect_to root_path
+  end
+  
+
+  private
+  
+  #StrongParamater
+  def task_params
+    params.require(:task).permit(:content)
+  end
+
+end
